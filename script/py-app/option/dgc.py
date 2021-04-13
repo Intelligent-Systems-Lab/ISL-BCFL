1b6fbfefdf8783d87fdffee5def6764e9141bca6
tony92151 <tony92151@gmail.com>
2021-04-13 11:48:40 +0800

    add correction

--- script/py-app/option/dgc.py
+++ script/py-app/option/dgc.py
@@ -10,6 +10,19 @@ class config_dgc:
     def get_compress_ratio(self) -> float:
         return float(self.config["compress_ratio"])
 
+    def get_fusing_ratio(self) -> float:
+        return float(self.config["fusing_ratio"])
+
+    def get_momentum(self) -> float:
+        return float(self.config["momentum"])
+
+    def get_momentum_correction(self) -> bool:
+        if self.config["momentum_correction"] == "False":
+            return False
+        elif self.config["momentum_correction"] == "True":
+            return True
+
+
     # def get_sample_ratio(self) -> float:
     #     return float(self.config["sample_ratio"])
 
